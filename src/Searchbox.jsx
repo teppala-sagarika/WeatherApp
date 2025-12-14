37a28f7cef3daa518bf92ac665ecc11a70aa5208
Teppala Sagarika <sagarikateppala@gmail.com>
2025-12-14 22:42:35 +0530

    Using Weather API

--- src/Searchbox.jsx
+++ src/Searchbox.jsx
@@ -5,6 +5,22 @@ import './Searchbox.css';
 
 export default function Searchbox(){
     let [city,setCity]=useState("");
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY=import.meta.env.VITE_API_KEY;
+    let generateWeatherData=async()=>{
+        let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse=await response.json();
+        console.log(jsonResponse);
+        let result={
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempmax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+        console.log(result);
+    }
     let handleChange=(event)=>{
         setCity(event.target.value);
     }
@@ -12,6 +28,7 @@ export default function Searchbox(){
         event.preventDefault();
         console.log(city);
         setCity("");
+        generateWeatherData();
     }
     return (<>
     <div className="searchBox">
