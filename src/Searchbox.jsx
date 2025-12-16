b7d38590f89941800df5f181010eeea0b1a07ae7
Teppala Sagarika <sagarikateppala@gmail.com>
2025-12-16 22:07:29 +0530

    Added error handling

--- src/Searchbox.jsx
+++ src/Searchbox.jsx
@@ -5,12 +5,14 @@ import './Searchbox.css';
 
 export default function Searchbox({updateInfo}){
     let [city,setCity]=useState("");
+    let [error,setError]=useState(false);
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
     const API_KEY=import.meta.env.VITE_API_KEY;
     let generateWeatherData=async()=>{
+        try{
         let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         let jsonResponse=await response.json();
-        console.log(jsonResponse);
+        //console.log(jsonResponse);
         let result={
             city: city,
             temp: jsonResponse.main.temp,
@@ -20,8 +22,11 @@ export default function Searchbox({updateInfo}){
             feelsLike: jsonResponse.main.feels_like,
             weather: jsonResponse.weather[0].description,
         };
-        console.log(result);
+        //console.log(result);
         return result;
+        }catch(err){
+            throw err;
+        }
     }
 
     let handleChange=(event)=>{
@@ -29,11 +34,16 @@ export default function Searchbox({updateInfo}){
     }
 
     let handleSubmit=async (event)=>{
+        try{
         event.preventDefault();
-        console.log(city);
+        //console.log(city);
+        let currentCity = city;   // save value
         setCity("");
-        let info = await generateWeatherData();
+        let info = await generateWeatherData(currentCity);
         updateInfo(info);
+        }catch(err){
+            setError(true);
+        }
     }
 
     return (<>
@@ -42,6 +52,7 @@ export default function Searchbox({updateInfo}){
             <TextField id="city" label="City Name" variant="outlined" value={city} onChange={handleChange} required/>
             <br/><br/>
             <Button variant="contained" type="submit">Search</Button>
+            {error && <p style={{color:"red"}}> No such place exists </p>}
         </form>
     </div>
     </>);
