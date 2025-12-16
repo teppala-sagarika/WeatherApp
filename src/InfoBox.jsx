485d3e6b43b10a74aa7540bbb58af3d45d46d6e6
Teppala Sagarika <sagarikateppala@gmail.com>
2025-12-16 22:39:08 +0530

    Updated card image based on weather conditions

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -5,12 +5,15 @@ import Typography from '@mui/material/Typography';
 import './InfoBox.css';
 export default function InfoBox({info}){
     let IMG_URL="https://images.unsplash.com/photo-1630260667842-830a17d12ec9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    let rainy="https://images.unsplash.com/photo-1558409057-bbe679023136?q=80&w=1337&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    let cold="https://images.unsplash.com/photo-1445543949571-ffc3e0e2f55e?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    let hot="https://plus.unsplash.com/premium_photo-1736338574940-03f94e4df965?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     return (
         <div className="InfoBox">
             <Card sx={{ maxWidth: 345 }} className='infocard'>
       <CardMedia
         sx={{ height: 140 }}
-        image={IMG_URL}
+        image={info.humidity>80?rainy:info.temp>15?hot:cold}
         title="weather today"
       />
       <CardContent>
