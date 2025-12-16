ce895483b22bb3863281c29a2489e4e03172ad10
Teppala Sagarika <sagarikateppala@gmail.com>
2025-12-16 22:46:38 +0530

    Updated city titles with icons based on weather conditions

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -3,6 +3,9 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import './InfoBox.css';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import SunnyIcon from '@mui/icons-material/Sunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 export default function InfoBox({info}){
     let IMG_URL="https://images.unsplash.com/photo-1630260667842-830a17d12ec9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     let rainy="https://images.unsplash.com/photo-1558409057-bbe679023136?q=80&w=1337&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
@@ -18,7 +21,7 @@ export default function InfoBox({info}){
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {info.city}
+          {info.city} {info.humidity>80?<ThunderstormIcon/>:info.temp>15?<SunnyIcon/>:<AcUnitIcon/>}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
             <p>Temperature = {info.temp}&deg;C</p>
